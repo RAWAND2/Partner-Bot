a232b3b41a7aed7c0ec0578f9620d1a49573f321
RAWAND2 <95169668+RAWAND2@users.noreply.github.com>
2022-08-08 22:57:58 +0300

    Update Ano.js

--- Ano.js
+++ Ano.js
@@ -58,10 +58,10 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(`  https://discord.gg/kq4KWJnBaw`) ///Link Server Never ///RaWeN
+  message.author.send(`https://discord.gg/WnpcyNzT8u`) ///Link Server Never ///Ranjo
     
-  client.channels.get("ID Channel").send(
-`> RaWeN Up | By: <@${message.author.id}> 
+  client.channels.get("https://discord.gg/WnpcyNzT8u").send(
+`> Ranjo Up | By: <@${message.author.id}> 
 ${message.content}`)
 }
     }
@@ -73,10 +73,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Send Me Linke Your Server | RaWeN Up`) 
+client.user.setGame(`Send Me Linke Your Server | Ranjo Up`) 
 });
 
 
 
 
-client.login("");///FiiX ///Token
+client.login("MTAwNjE3NDc4NDUyNDEzMjQ2Mw.GlbXrg.IAKWrFiZ_8_0_V4kDH6LoerwfmbwlVNevxM_UM");///FiiX ///Token
